6981baacf09ae7654da08f073a0d6c9cb65ff696
Arpasara Rungrueng <Arpasara25@gmail.com>
2021-03-17 17:15:06 +0700

    add func file

--- hw2/func.php
+++ hw2/func.php
@@ -36,23 +36,46 @@
 // echo calnumber(num1, $num2);
 // echo "<br>";
 
-switch ($score) {
-    case $score > 79;
-        $gpa = "A";
+
+function GPA($score = 56)
+{
+switch ($score) 
+{
+
+    case ($score < 50):
+        echo "F";
+       break;
+    case ($score <55):
+        echo "D";
         break;
-    case ($score > 69 && $score < 80);
-        $gpa = "B"
+    case ($score <60):
+        echo "D+";
         break;
-    case ($score > 59 && $score < 70);
-        $gpa = "C"
+    case ($score <65):
+        echo "C";
         break;
-    case ($score > 49 && $score < 60);
-        $gpa = "D"
+    case ($score <70):
+        echo "C+";
+        break; 
+    case ($score <75):
+        echo "B";
+        break;   
+        
+    case ($score < 80):
+        echo "B+";
         break;
-    default;
-        $gpa "F"
+    default:
+        echo "A";
         break;
     }
+    echo "<br>";
+    return $score;
+
+}
+echo "<br>";
+echo GPA()
+
+?>
 
 
 //{ 
