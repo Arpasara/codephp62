f54a07a467f28e70e8aa82e894e15e8c03359a73
Arpasara Rungrueng <Arpasara25@gmail.com>
2021-03-17 17:00:18 +0700

    add func file

---
+++ hw2/func.php
@@ -0,0 +1,82 @@
+<?php
+// myFunction();
+
+//function myFunction()
+//{
+//    echo "my1";
+//}
+//$srt = functon2();
+
+//$srt += 1;
+//echo $srt;
+//function functon2()
+//{
+//    echo "my"."<br>";
+
+//    return "you call me";
+//}
+//$str = functon2();
+//echo $str;
+//$calFunc = functon2();
+
+//$calFunc;
+
+//function calnumber(&$num1, $num2 = 7)
+//{
+//    $a = $num1;
+//    $b = $num2;
+//    $c = $a + $b;
+//    $num1 = 6;
+//    return $c;
+//}
+//$num1 = 1;
+//echo "<br>";
+//$num1 = 1;
+//$num2 = 6; 
+// echo calnumber(num1, $num2);
+// echo "<br>";
+
+switch ($score) {
+    case $score > 79;
+        $gpa = "A";
+        break;
+    case ($score > 69 && $score < 80);
+        $gpa = "B"
+        break;
+    case ($score > 59 && $score < 70);
+        $gpa = "C"
+        break;
+    case ($score > 49 && $score < 60);
+        $gpa = "D"
+        break;
+    default;
+        $gpa "F"
+        break;
+    }
+
+
+//{ 
+//    if ($num == 0 || $num == 1) {
+//        return 1;
+//    } else {
+//       return $num * fatorail($num - 1);
+        
+//    }
+//}
+//echo fatorail(5) . "<br>";
+// echo fatorail(5) . "<br>";
+
+// $Fullname = "Mr.Suradet Petcharanon";
+// $lastname = substr($Fullname,11,3);
+// echo $lastname;
+// echo strlen($lastname);
+// echo "<br>";
+// echo max(1,4.33,45);
+// echo round(19.987777,2);
+
+//echo date("d/m/Y", "1579737600");
+//echo "<br>";
+// echo getdate();
+//$currentdate = getdate();
+//echo $currentdate["mday"];
+//echo time();
